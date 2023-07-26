81f46e14c1d86311ab11e0707a7a7b4962dbb374
paulrichardjacob <paul_richard_jacob@outlook.com>
2023-07-26 12:22:22 +0100

    Added def sample and extend

--- final.py
+++ final.py
@@ -14,6 +14,37 @@ def main():
 
     # Load Panda robot
     robot = p.loadURDF("franka_panda/panda.urdf", [0, 0, 0], useFixedBase=True)
+    joint_limits = []
+    num_joints = p.getNumJoints(robot)
+
+    for i in range(num_joints):
+        info = p.getJointInfo(robot, i)
+        joint_limits.append((info[8], info[9]))  # lower and upper limit for each joint
+
+    joint_limits = np.array(joint_limits)  # Convert to numpy array for convenience
+    print(joint_limits)
+    def sample_fn():
+        q = np.zeros(num_joints)
+        for i in range(num_joints):
+            lower_limit, upper_limit = joint_limits[i]
+            if np.isinf(lower_limit) or np.isinf(upper_limit):  # It's a continuous joint
+                q[i] = np.random.uniform(-np.pi, np.pi)  # Sample from -pi to pi
+            elif lower_limit == upper_limit:  # It's a fixed joint
+                q[i] = lower_limit  # Or upper_limit, they're the same
+            else:  # Regular joint
+                q[i] = np.random.uniform(lower_limit, upper_limit)
+        return q
+
+    def extend_fn(start_conf, end_conf):
+        num_steps = 100  # The number of intermediate steps between start_conf and end_conf
+        for i in range(num_steps):
+            t = float(i) / (num_steps - 1)  # The fraction of the way from start_conf to end_conf
+            interpolated_conf = (1 - t) * np.array(start_conf) + t * np.array(end_conf)
+            # Now you need to compute the IK solution to check if this configuration is possible
+            joint_positions = p.calculateInverseKinematics(robot, 11, interpolated_conf[:3], interpolated_conf[3:])
+            # Validate if this position is in collision
+            if not is_in_collision(joint_positions):
+                yield joint_positions
 
     def set_initial_pose(robot, desired_pose):
         # Convert the pose to a position and a quaternion
